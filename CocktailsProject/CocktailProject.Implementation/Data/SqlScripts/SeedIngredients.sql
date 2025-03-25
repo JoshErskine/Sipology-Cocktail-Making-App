805a9ea2d6331af65033e73bb3ef9af59a77f46a
Josh <128964404+JoshErskine@users.noreply.github.com>
2025-03-25 20:01:18 +0000

    Seed Ingredients SQL script

---
+++ CocktailsProject/CocktailProject.Implementation/Data/SqlScripts/SeedIngredients.sql
@@ -0,0 +1,12 @@
+INSERT INTO Ingredient (GlobalId, Name, DateCreated, DateModified)
+VALUES
+    ('b1a6e7c2-45f3-4b71-b7d8-0a5f2c1e3d9a', 'Tequila', '2025-02-19', '2025-02-19'),
+    ('c2b8f914-7e5a-4e31-bc1d-95f6a8c52b3e', 'Rum', '2025-02-19', '2025-02-19'),
+    ('d4e10c75-f1aa-4d89-b624-7f47b0f88e72', 'Coconut Cream', '2025-02-19', '2025-02-19'),
+    ('e6f8b927-6e32-437c-ae99-d3c9f80f4b5c', 'Lime Juice', '2025-02-19', '2025-02-19'),
+    ('a3d5c2e1-8f72-4e3b-b1cd-4a7f9e20d5b9', 'Sugar Syrup', '2025-02-19', '2025-02-19'),
+    ('f9c2d4b7-3e4a-4b80-b8f6-d2a1c9e5f710', 'Whiskey', '2025-02-19', '2025-02-19'),
+    ('b2e6f3d7-1a9c-4e75-a23d-7c58f0b9d2e4', 'Cranberry Juice', '2025-02-19', '2025-02-19'),
+    ('c8a1d5f2-7e4b-4b3c-b10d-f729e6c4d5b8', 'Pineapple Juice', '2025-02-19', '2025-02-19'),
+    ('d9b3c8f6-5a71-4e24-bc02-7e1f5d9a3c6b', 'Mint Leaves', '2025-02-19', '2025-02-19'),
+    ('e4d2c1b5-8f6a-4b7c-a390-d7f1e52b4c8a', 'Triple Sec', '2025-02-19', '2025-02-19');
